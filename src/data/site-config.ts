9653eae280f7710ffd1af41de6a6a79d294077ef
FullstackSena <fullstackcolegiosena@gmail.com>
2024-09-27 13:32:58 -0500

    Primer Commit

---
+++ src/data/site-config.ts
@@ -0,0 +1,121 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'Dayana Erazo',
+    subtitle: 'Abogada especialista',
+    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
+    },
+    headerNavLinks: [
+        {
+            text: 'Inicio',
+            href: '/'
+        },
+        {
+            text: 'Experiencia laboral',
+            href: '/projects'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'Sobre mi',
+            href: '/about'
+        }
+    ],
+    footerNavLinks: [
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+        {
+            text: 'Terms',
+            href: '/terms'
+        },
+        {
+            text: 'Download theme',
+            href: 'https://github.com/JustGoodUI/dante-astro-theme'
+        }
+    ],
+    socialLinks: [
+        {
+            text: 'Dribbble',
+            href: 'https://dribbble.com/'
+        },
+        {
+            text: 'Instagram',
+            href: 'https://instagram.com/'
+        },
+        {
+            text: 'X/Twitter',
+            href: 'https://twitter.com/'
+        }
+    ],
+    hero: {
+        title: 'Hola, bienvenidos a mi web!',
+        text: "Soy Dayana, <b>abogada</b> con especialización en Derecho Administrativo, apasionada por la intersección entre la lectura jurídica y la tecnología aplicada al Derecho. Mi compromiso, capacidad de adaptación y deseo de aportar de manera continua a mi entorno me impulsan a buscar soluciones innovadoras dentro de este campo. Con una firme convicción en la importancia de la evolución tecnológica en el ámbito legal, busco contribuir activamente al desarrollo de prácticas más eficientes y accesibles.",
+        image: {
+            src: '/hero.jpeg',
+            alt: 'A person sitting at a desk in front of a computer'
+        },
+        actions: [
+            {
+                text: 'Ponte en contacto',
+                href: '/contact'
+            }
+        ]
+    },
+    subscribe: {
+        title: 'Subscribe to Dante Newsletter',
+        text: 'One update per week. All the latest posts directly in your inbox.',
+        formUrl: '#'
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
